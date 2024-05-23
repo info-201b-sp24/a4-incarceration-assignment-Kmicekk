11aae95a8147f837b75dc1c9a351f91ee2988ab1
Kmicekk <91647494+Kmicekk@users.noreply.github.com>
2024-05-23 16:08:46 -0700

    new

--- summary.R
+++ summary.R
@@ -2,4 +2,16 @@ average_incarceration_rate <- inc_data %>%
   group_by(year) %>%
   summarise(average_rate = mean(total_pop, na.rm = TRUE))
 
-print(average_incarceration_rate)
+average_incarceration_rate
+
+high_aapi_rate <- inc_data %>%
+  group_by(state) %>%
+  summarise(high_rate <- max(aapi_pop_15to64, na.rm = T))
+
+high_aapi_rate
+
+high_black_rate <- inc_data %>%
+  group_by(state) %>%
+  summarise(high_rate <- max(black_pop_15to64, na.rm = T))
+
+high_black_rate
